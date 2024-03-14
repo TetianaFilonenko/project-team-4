759fd8f9d34f9da8bb8677fa32bb5124efa5d7bf
Andrsi13 <vladika.andrsi@gmail.com>
2024-03-14 11:26:56 +0200

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='console_bot',
+      version='1',
+      description='Console bot that will help you with everything',
+      url='https://github.com/TetianaFilonenko/project-team-4.git',
+      author='Kobzar Coders',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages= find_namespace_packages())
