c6c7ead5d2d4f7499002c646284188d05ae92c4e
OF3000 <ofpost@i.ua>
2024-03-14 23:38:03 +0200

    added edit recor command

--- console_bot.py
+++ console_bot.py
@@ -37,6 +37,7 @@ Available commands (->/right click is used for autocomplete a command):
   close/exit                                     - Exits the program.
   random-book                                    - Generate random book with 10 contacts.
   random-note                                    - Generate random note from Taras Hryhorovych Shevchenko poem
+  edit [fieldname] [name]                        - Edit/Delete phone, email, address.
 """
     print(help_text)
 
@@ -67,9 +68,19 @@ commands = [
     "exit",
     "random-book",
     "random-note",
+    "edit",
 ]
 style = Style.from_dict({"": "#1cb649 italic bold"})
 
+def get_index(field,len):
+    index = input(f"Enter a index of {field}(eg. 1 for first): ")
+    if index.isnumeric():
+        index = int(index)
+    else: index = 0
+    if index > len or not index:
+        print("Invalid index, error executing edit command, try again")
+        index = 0
+    return index
 
 def main():
     """Central function printing all the commands"""
@@ -152,6 +163,59 @@ def main():
             print(input_manager.all_notes())
         elif command == "random-note":
             print_note(input_manager.random_note())
+        elif command == 'edit':
+            if len(args) < 2:
+                print("Invalid input, expected 'edit field recordname'")
+                continue
+            field = args[0]
+            args.pop(0)
+            if not input_manager.get_contact_phone(args):
+                print(f"{args[0]} not found")
+                continue
+            record = input_manager.book[args[0]]
+
+            if field == "phone":
+                print(input_manager.get_contact_phone(args))
+                if not record.phones:
+                    continue
+                index = get_index("phone", len(record.phones))
+                if not index:
+                    continue
+                nphone = input("Enter new phone or enter 'delete': ")
+                if nphone == 'delete':
+                    record.phones.pop(index-1)
+                    print("phone has been deleted")
+                else:
+                    print(input_manager.change_contact([args[0], str(record.phones[index-1]), nphone]))                    
+            
+            elif field == "email":
+                print(input_manager.get_contact_email(args))
+                if not record.emails:
+                    continue
+                index = get_index("email", len(record.emails))
+                if not index:
+                    continue
+                nemail = input("Enter new email or enter 'delete': ")
+                if nemail == 'delete':
+                    record.emails.pop(index-1)
+                    print("email has been deleted")                    
+                else:
+                    print(input_manager.change_contact_email([args[0], str(record.emails[index-1]), nemail]))
+            
+            elif field == "address":
+                print(input_manager.get_contact_address(args))
+                if not record.addresses:
+                    continue
+                index = get_index("address", len(record.addresses))
+                if not index:
+                    continue
+                naddr = input("Enter new address or enter 'delete': ")
+                if naddr == 'delete':
+                    record.addresses.pop(index-1)
+                    print("address has been deleted")
+                else:
+                    print(input_manager.change_contact_address([args[0], str(record.addresses[index-1])], naddr))       
+            else: print("not supported field")
         else:
             print("Invalid command.")
         input_manager.save_to_json()
