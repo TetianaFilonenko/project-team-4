b9750b6bf1fca4a4d9d49a7ab958f9a34d0dd9a1
OleksandrYagelskiy <156130804+OleksandrYagelskiy@users.noreply.github.com>
2024-03-14 10:26:28 +0200

    test 01

--- console_bot/input_manager.py
+++ console_bot/input_manager.py
@@ -2,6 +2,30 @@ from .errors import input_error
 from .note import NoteBook, Note
 from .address_book import Record, AddressBook
 import json
+import os
+
+class AddressBook(UserDict): 
+    def save_to_file(self, filename): 
+        with open(filename, 'w') as file:
+            json.dump(self.to_dict(), file)
+
+    @classmethod
+    def load_from_file(cls, filename):
+        if os.path.exists(filename):
+            with open(filename, 'r') as file:
+                data = json.load(file)
+            return cls.from_dict(data)
+        else:
+            return cls()
+        
+# Приклад використання:
+address_book = AddressBook()
+address_book.generate_random_data()
+address_book.save_to_file('address_book.json')
+
+# Після перезапуску програми:
+address_book_loaded = AddressBook.load_from_file('address_book.json') #зберігає дані контактної книги у файл address_book.json
+print(address_book_loaded)
 
 
 class InputManager:
@@ -122,3 +146,5 @@ class InputManager:
             json_data = json.load(fh)
             self.book = AddressBook.from_dict(json_data)
         return "Restoring is done"
+    
+
