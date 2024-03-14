5eec2f694788408c25bc24c03a90e0f29fc631d3
OleksandrYagelskiy <156130804+OleksandrYagelskiy@users.noreply.github.com>
2024-03-14 09:55:49 +0200

    Update address_book.py
    з 317 строки

--- console_bot/address_book.py
+++ console_bot/address_book.py
@@ -314,6 +314,19 @@ class AddressBook(UserDict):
     def to_dict(self):
         return {key: value.to_dict() for key, value in self.data.items()}
 
+    def save_to_file(self, filename): 
+        with open(filename, 'w') as file:
+            json.dump(self.to_dict(), file)
+            
+    @classmethod
+    def load_from_file(cls, filename):
+        if os.path.exists(filename):
+            with open(filename, 'r') as file:
+                data = json.load(file)
+            return cls.from_dict(data)
+        else:
+            return cls()
+    
     @classmethod
     def from_dict(cls, dict_data):
         address_book = cls()
